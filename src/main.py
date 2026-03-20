da1162b40f75370dce93de680c352a5f895d6bb3
Michael <Michael.A.Manos@gmail.com>
2026-03-20 16:34:56 -0700

    5.5.1 Add static content

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ def main():
     base_path = "/"
     if len(sys.argv) > 1:
         base_path = sys.argv[1]
-    utils.copy_folder("static", "public")
+    utils.copy_folder("static", "docs")
     generator.generate_pages_recursive("content", "template.html", "docs", base_path)
 
 
